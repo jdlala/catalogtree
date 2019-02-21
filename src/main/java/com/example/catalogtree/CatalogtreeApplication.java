bec08df211960d152373cc4be80ba52187a0185e
jdlala <732148342@qq.com>
2019-02-21 11:38:54 +0800

    创建目录树项目

---
+++ src/main/java/com/example/catalogtree/CatalogtreeApplication.java
@@ -0,0 +1,13 @@
+package com.example.catalogtree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogtreeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatalogtreeApplication.class, args);
+    }
+
+}
